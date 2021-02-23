55a21aacf797b24dce8ad350e7e71fec19e58419
Jaime Pastrana García <jaipas01@ucm.es>
2021-02-23 10:30:39 +0100

    Archivos iniciales

---
+++ calculadora.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Introduzca dos nºs: ";
+	int a, b;
+	cin >> a >> b,
+	cout << "La suma de " << a << " y " << b << " da como resultado" << a+b;
+}
