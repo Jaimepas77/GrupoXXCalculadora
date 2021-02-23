c39f25c55af4e1daa8aa918db8afa74d92798f16
Jaime Pastrana García <jaipas01@ucm.es>
2021-02-23 19:47:51 +0100

    Funcionalidades de resta, multiplicacion y division añadidas. Ademas se crean funciones para cada una de las tareas.

--- calculadora.cpp
+++ calculadora.cpp
@@ -2,10 +2,38 @@
 
 using namespace std;
 
+int suma(int a, int b)
+{
+	return a + b;
+}
+
+int resta(int a, int b)
+{
+	return a - b;
+}
+
+int multiplicacion(int a, int b)
+{
+	return a*b;
+}
+
+int division(int a, int b)
+{
+	return a/b;
+}
+
+int resto(int a, int b)
+{
+	return a%b;
+}
+
 int main()
 {
 	cout << "Introduzca dos nums: ";
 	int a, b;
 	cin >> a >> b,
-	cout << "La suma de " << a << " y " << b << " da como resultado " << a+b << endl;
+	cout << "La suma de " << a << " y " << b << " da como resultado " << suma(a, b) << endl;
+	cout << "La resta de " << a << " y " << b << " da como resultado " << resta(a, b) << endl;
+	cout << "La multiplicacion de " << a << " y " << b << " da como resultado " << multiplicacion(a, b) << endl;
+	cout << "La division de " << a << " y " << b << " da como resultado " << division(a, b) << " y como resto " << resto(a, b) << endl;
 }
