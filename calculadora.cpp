8f1cc923d0152ed3c55d6af73731860d3e0cd354
Jaime <jaipas01@ucm.es>
2022-11-01 19:49:52 +0100

    PRobando commit con descripción.
    Descripción super guay

--- calculadora.cpp
+++ calculadora.cpp
@@ -36,4 +36,5 @@ int main()
 	cout << "La resta de " << a << " y " << b << " da como resultado " << resta(a, b) << endl;
 	cout << "La multiplicacion de " << a << " y " << b << " da como resultado " << multiplicacion(a, b) << endl;
 	cout << "La division de " << a << " y " << b << " da como resultado " << division(a, b) << " y como resto " << resto(a, b) << endl;
+	cout << "Fin";
 }
